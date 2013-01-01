a2ca48168b24a0e196936f4ad29ee9d63077567b
cash <cash924n@gmail.com>
2013-01-01 23:16:35 +0800

    Remove redundant function declaration

--- src/DataMgrFactory.h
+++ src/DataMgrFactory.h
@@ -13,12 +13,7 @@ namespace util
 		static IDataManager* GetInstance(EnDataMgr enParser = BoostJson);
 		static int DeleteInstance(IDataManager* pIDataMgr); 
 
-		static void RemoveSingleInstanceFromMap( map<EnDataMgr,IDataManager*>::iterator& it );
-
-
-
 	private:
-		static void CleanSingleInstance( IDataManager* pIDataMgr , int& nResuilt );
 		static map<EnDataMgr, IDataManager*> S_MAP_DATAMGR;
 	};
 }
